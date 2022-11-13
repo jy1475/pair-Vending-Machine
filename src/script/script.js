2014af141f85d89aec90764454c3348ae667af6e
JuYeol Kim <112503881+jy1475@users.noreply.github.com>
2022-11-13 19:44:36 +0900

    기능구현 완성

--- src/script/script.js
+++ src/script/script.js
@@ -1,274 +1,243 @@
-// **** 변수선언 ****
-let drinks = [
-    {
-        "name": "Original_Cola",
-        "img": "./src/img/Original_Cola.png",
-        "price": 1000,
-        "count": 0,
-        "stock": 3
-    },
-    {
-        "name": "Violet_Cola",
-        "img": "./src/img/Violet_Cola.png",
-        "price": 1000,
-        "count": 0,
-        "stock": 3
-    },
-    {
-        "name": "Yellow_Cola",
-        "img": "./src/img/Yellow_Cola.png",
-        "price": 1000,
-        "count": 0,
-        "stock": 3
-    },
-    {
-        "name": "Cool_Cola",
-        "img": "./src/img/Cool_Cola.png",
-        "price": 1000,
-        "count": 0,
-        "stock": 3
-    },
-    {
-        "name": "Green_Cola",
-        "img": "./src/img/Green_Cola.png",
-        "price": 1000,
-        "count": 0,
-        "stock": 3
-    },
-    {
-        "name": "Orange_Cola",
-        "img": "./src/img/Orange_Cola.png",
-        "price": 1000,
-        "count": 0,
-        "stock": 3
-    }
-];
+let 음료 = [{
+    "이름": "Original_Cola",
+    "가격": 1000,
+    "이미지":"./src/img/Original_Cola.png",
+    "선택개수": 0,
+    "재고":3, 
+},
+{
+    "이름": "Violet_Cola",
+    "가격": 1000,
+    "이미지": "./src/img/Violet_Cola.png",
+    "선택개수": 0,
+    "재고": 3, 
+},
+{
+    "이름": "Yello_Cola",
+    "가격": 1000,
+    "이미지":"./src/img/Yellow_Cola.png",
+    "선택개수": 0,
+    "재고": 3, 
+},
+{
+    "이름": "Cool_Cola",
+    "가격": 1000,
+    "이미지":"./src/img/Cool_Cola.png",
+    "선택개수": 0,
+    "재고": 3, 
+},{
+    "이름": "Green_Cola",
+    "가격": 1000,
+    "이미지":"./src/img/Green_Cola.png",
+    "선택개수": 0,
+    "재고": 3, 
+},{
+    "이름":"Orange_Cola",
+    "가격":1000,
+    "이미지": "./src/img/Orange_Cola.png",
+    "선택개수":0,
+    "재고":3 , 
+}];
 
-let walletCoin = 25000; //소지금
-let inputCoin = 10000; // 입금액
-let totalPrice = 0; //총 금액 (총금엑 += 음료가격 * 음료 개수)
-let itemNum = drinks.length; // 음료의 개수
-let balance = 0; //잔액
+let 입금액 = 0;
+let 소지금액 = 25000;
+let 잔액 = 2000;
+let 총금액 = 0; // 선택한 음료 * 가격의 합
 
+// DOM API
+const 장바구니 = document.querySelector(".Cola_Select");
+const 상품 = 장바구니.querySelectorAll(".Cola_label_cont");
+const 가격표 = 장바구니.querySelectorAll(".Cola_Price");
+const 상품이미지 = 장바구니.querySelectorAll(".Cola_List_img");
+const 입금액입력 = document.querySelector('.Deposit');
+const 입금버튼 = document.querySelector('.Deposit_btn');
+const 잔액디스플레이 = document.querySelector('.won');
+const 소지금디스플레이 = document.querySelector('.fund_won');
+const 거스름돈버튼 = document.querySelector('.Change_btn');
+const 총금액디스플레이 = document.querySelector('.extended_price');
+const 획득버튼 = document.querySelector('.Coke_get_btn');
+const 획득음료목록 = document.querySelector('.Coke_list_ls');
+const 선택리스트 = document.querySelector(".Coke_list");
 
-// **** drinks 객체 디스플레이 ****
-let domFrag = document.createDocumentFragment();
-for (let i = 0; i < itemNum; i++) {
-    let item_Li = document.createElement("li");
-    let item_Img = document.createElement("img");
-    let p_itemName = document.createElement("p");
-    let span_priceTag = document.createElement("span");
-    item_Li.setAttribute("class", "item-li");
-    item_Img.setAttribute("class", "items-img");
-    item_Img.setAttribute("src", drinks[i].img);
-    item_Img.setAttribute("alt", drinks[i].name);
-    p_itemName.setAttribute("class", "items-name");
-    span_priceTag.setAttribute("class", "items-pricetag");
-    p_itemName.textContent = drinks[i].name;
-    span_priceTag.textContent  = drinks[i].price + "원"
-    item_Li.appendChild(item_Img);
-    item_Li.appendChild(p_itemName);
-    item_Li.appendChild(span_priceTag);
-    domFrag.appendChild(item_Li);
+for (let i=0; i < 음료.length; i++){
+    let 임시상품이미지 = 음료[i].이미지;
+    let 임시이름 = 음료[i].이름;
+    let 임시가격표 = 음료[i].가격;
+    상품[i].textContent = 임시이름;
+    가격표[i].textContent = 임시가격표 + "원";
+    상품이미지[i].src = 음료[i].이미지;
+    상품이미지[i].alt = 음료[i].이름;
 }
-document.querySelector(".machine-items-list").appendChild(domFrag);
 
+function 선택취소(해당태그){
+    const 해당태그초이스 = 해당태그.querySelector(".choice");
+    let index = 음료.indexOf(음료.filter(i => i["이름"] == 해당태그초이스.id)[0]);
+    if(음료[index].선택개수 > 1){
+        // 객체에서 개수-1
+        음료[index].선택개수 -= 1;
+        음료[index].재고 += 1;
+        
 
-// **** DOM API ****
-const machineItems = document.querySelector(".machine-items");
-const machineFunc = document.querySelector(".machine-func");
-const pocket = document.querySelector(".pocket");
-const itemList = [...document.querySelectorAll(".item-li")];
-const inputBtn = machineFunc.querySelector(".btn-payment"); // 입금버튼
-const changeBtn = machineFunc.querySelector(".btn-change"); // 거스름돈 반환 버튼
-const getBtn = machineFunc.querySelector(".btn-get"); // 획득 버튼
+        // -1된 개수를 다시 찍어주는거
+        해당태그초이스.innerHTML = 음료[index].선택개수;
+    }
+    // 원래 개수가 1일 때 아예 리스트가 없어지고 선택개수도 -1
+    else if (음료[index].선택개수 == 1){
+        // 객체에서 개수-1
+        음료.filter(i => i["이름"] == 해당태그초이스.id)[0]['선택개수'] -= 1;
+        음료[index].재고 += 1;
+        // 아예 li 삭제
+        해당태그.parentNode.removeChild(해당태그);
+    }
+    for (let i=0; i<상품목록.length; i++){
+        if (음료[i].재고 === 0){
+            상품목록[i].classList.add("sold_out");
+        }
+        else if (음료[i].재고 > 0){
+            상품목록[i].classList.remove("sold_out");
+        }
+    }
+    alert("취소!");
+}
 
-
-// **** 이벤트 추가 ****
-itemList.forEach((item, index) => {
-    item.addEventListener("click", ()=>{
-        selectItem(index);
-    });
+// 이벤트 달기
+획득버튼.addEventListener("click", ()=>{
+    획득();
 })
-inputBtn.addEventListener("click", deposit);
-changeBtn.addEventListener("click", changeBalance);
-getBtn.addEventListener("click", getDrink);
 
+자금셋팅();
 
-// **** 함수선언문 ****
-// 금액 업데이트 함수 update()
-function update(){
-    // 잔액을 찍어주는 코드
-    machineFunc.querySelector(".balance_result").textContent = balance.toLocaleString() + "원";
-
-    // 소지금을 찍어주는 코드
-    pocket.querySelector(".wallet_coin").textContent = walletCoin.toLocaleString() + "원";
-
-    // 총금액을 찍어주는 코드
-    pocket.querySelector(".total-price").textContent =
-        "총금액 : " + totalPrice.toLocaleString() + "원";
-}
-update();
-// 입금 기능 함수 deposit()
-function deposit() {
-    inputCoin = parseInt(
-        machineFunc.querySelector(".input-payment").value
-    );
-    if (!(inputCoin)) {
-        alert("금액을 입력해주세요!");
+입금버튼.addEventListener('click', () => {
+    if (입금액입력.value > 소지금액){
+        alert("소지금 부족!");
         return;
     }
-    if (inputCoin > walletCoin) {
-        alert("소지금보다 많은 금액을 입금할 수 없습니다!");
+    let 입금액 = +입금액입력.value;
+    잔액 += 입금액;
+    소지금액 -= 입금액;
+    자금셋팅();
+    입금액입력.value=""; 
+})
+
+거스름돈버튼.addEventListener('click', ()=>{
+    if (잔액 === 0){
+        alert("거스름돈없어!");
         return;
     }
-    balance += inputCoin;
-    // 입금 시 소지금에서 차감
-    walletCoin -= inputCoin;
-    // 인풋창 초기화
-    machineFunc.querySelector(".input-payment").value = null;
-    update();
-}
+    소지금액+=잔액;
+    잔액 = 0;
+    자금셋팅();
+})
 
-// 거스름돈 반환기능 함수 changeBalance()
-function changeBalance() {
-    walletCoin += balance;
-    balance = 0;
-    update();
-}
+//클릭이벤트
+// 콜라들 -> querySelectorAll로 받아오고 -> click event
+const 상품목록 = 장바구니.querySelectorAll(".Cola_List");
+상품목록.forEach((i,index)=>{
+    i.addEventListener("click",()=>{
+        상품선택(index);
+    })
+})
 
-// 선택 음료 카운트하는 함수 selectItem(id)
-function selectItem(id) { 
-    //재고가 없을 땐 선택할 수 없도록 예외처리
-    if (drinks[id].stock < 1) {
-        alert("재고가 없습니다!")
+function 상품선택(index){
+    if(잔액<음료[index].가격){
+        alert("잔액을 넣어줘!");
         return;
     }
-    if (balance < drinks[id].price){
-        alert("잔액이 부족하여 상품을 담을 수 없습니다!");
+    // 음료 담겼을 때 갱신된 잔액 = 잔액 - 금액계산()
+    if (잔액 - 금액계산() < 음료[index].가격){
+        alert("상품을 담을 수 없습니다!");
         return;
     }
-    drinks[id].count += 1;
 
-    if (drinks[id].count > 1) {
-        // 이미 선택되어 있다면 새로 생성하지 않고 카운트만 증가
-        document.getElementById(drinks[id].name).innerHTML = drinks[id].count;
-        drinks[id].stock -= 1;
+    if(음료[index].재고<1){
+        alert("재고없음!");
+        return;
+    }
+    음료[index].선택개수 += 1;
+    // 이미 선택돼서 카운트만 늘려줘야 할 때
+    
+    if (음료[index].선택개수 > 1){
+        document.getElementById(음료[index].이름).innerHTML = 음료[index].선택개수;
+        음료[index].재고 -= 1;
     }
     else {
-        addtoSelectedList(id);
-        drinks[id].stock -= 1;
+        선택상품추가(index);
+        음료[index].재고 -= 1;
     }
-    // 재고(stock)가 0이 되면 품절 마크를 표시
-    let item_list = machineItems.getElementsByClassName("item-li");
-    for (let i = 0; i < itemNum; i++) {
-        if (drinks[i].stock == 0) {
-            item_list[i].classList.add("soldout");
+    
+    for (let i=0; i<상품목록.length; i++){
+        if (음료[i].재고 === 0){
+            상품목록[i].classList.add("sold_out");
         }
     }
-    balance -= drinks[id].price;
-    update();
 }
-
-// 카운트한 음료수를 출력하는 함수 addtoSelectedList(id)
-function addtoSelectedList(id) {
-    let selectarea = document.getElementById("selectarea");
-    //      <li class="get-drinks">
-    //          <p class="get-drinks-item">
-    //              <img
-    //                  src="./mediaquery/Original_Cola.png"
-    //                  alt="Original Cola"
-    //              />
-    //              <span class="get-drinks-item-name">Original_Cola</span>
-    //          </p>
-    //          <span class="count">1</span>
-    //      </li>
-    const liTag = document.createElement("li");
-    const pTag = document.createElement("p");
-    const imgTag = document.createElement("img");
-    const span1Tag = document.createElement("span");
-    const span2Tag = document.createElement("span");
-    liTag.setAttribute("class", "get-drinks");
-    pTag.setAttribute("class", "get-drinks-item");
-    imgTag.setAttribute("src", drinks[id].img);
-    imgTag.setAttribute("alt", drinks[id].name);
-    span1Tag.setAttribute("class", "get-drinks-item-name");
-    span2Tag.setAttribute("class", "count");
-    span2Tag.setAttribute("id", drinks[id].name);
-    span1Tag.innerHTML = drinks[id].name;
-    span2Tag.innerHTML = drinks[id].count;
-    liTag.appendChild(pTag);
-    pTag.appendChild(imgTag);
-    pTag.appendChild(span1Tag);
-    liTag.appendChild(span2Tag);
-    liTag.addEventListener("click", () => {
-        cancel(liTag);
+function 선택상품추가(index){
+    const 부모태그 = document.querySelector(".Coke_list");
+    /* <li>
+    <img src="./src/img/Original_Cola_s.png" alt="">
+    <span class="Cola_label">Original_Cola</span>
+    <span class="choice">0</span>
+    </li> */
+    const 상품태그 = document.createElement("li");
+    const 상품이미지태그 = document.createElement("img");
+    const 스팬태그1 = document.createElement("span");
+    const 스팬태그2 = document.createElement("span");
+    상품이미지태그.setAttribute('src', 음료[index].이미지);
+    상품이미지태그.setAttribute('alt', 음료[index].이름);
+    스팬태그1.textContent = 음료[index].이름;
+    스팬태그2.textContent = 음료[index].선택개수;
+    스팬태그1.setAttribute("class", "Cola_label");
+    스팬태그2.setAttribute("class", "choice");
+    스팬태그2.setAttribute("id", 음료[index].이름);
+    상품태그.appendChild(상품이미지태그);
+    상품태그.appendChild(스팬태그1);
+    상품태그.appendChild(스팬태그2);
+    
+    상품태그.addEventListener("click", () => {
+            선택취소(상품태그);
     });
-    selectarea.appendChild(liTag);
+    부모태그.appendChild(상품태그);
 }
 
-// 획득 기능 함수 getDrink()
-function getDrink() {
-    let selectOl = machineFunc.querySelector(".selected-list");
-    let getOl = pocket.querySelector(".selected-list");
-    let getDrinkList = [];
-    let getListCnt = pocket.querySelectorAll(".count");
-    [...getListCnt].forEach(i=>{
-        getDrinkList.push(i.id);
+function 금액계산(){
+    let 장바구니금액 = 0;
+    for (let i = 0; i < 음료.length; i++){
+        장바구니금액 += (음료[i].선택개수 * 음료[i].가격);
+    }
+    return 장바구니금액;
+}
+
+function 획득(){
+    let 획득음료배열 = [];
+    let 획득음료카운트 = 획득음료목록.querySelectorAll(".choice");
+    획득음료카운트.forEach(i=>{
+        획득음료배열.push(i.id);
     })
-    let selectList = machineFunc.querySelectorAll(".get-drinks");
-    [...selectList].forEach(i => {
-        let countTag = i.querySelector(".count");
-        if (getDrinkList.includes(countTag.id)){
-            pocket.querySelector(`#${countTag.id}`).innerHTML = ~~(pocket.querySelector(`#${countTag.id}`).innerHTML) + 1;
+    let 선택음료배열 = [...선택리스트.querySelectorAll("li")];
+    선택음료배열.forEach(i=>{
+        let 초이스태그 = i.querySelector(".choice");
+        if (획득음료배열.includes(초이스태그.id)){
+            획득음료목록.querySelector(`#${초이스태그.id}`).innerHTML = ~~(획득음료목록.querySelector(`#${초이스태그.id}`).innerHTML) + 1;
         }
         else {
-            getOl.appendChild(i);
+            획득음료목록.appendChild(i);
         }
     });
-    selectOl.innerHTML = "";
-    totalPrice += calTotalPrice();
-    // balance -= calTotalPrice();
-    update();
-    for (let i=0; i < itemNum; i++){
-        drinks[i].count = 0;
+
+    선택리스트.innerHTML = '';
+    총금액 += 금액계산();
+    잔액 -= 금액계산();
+    자금셋팅();
+    
+    // 선택 개수 다 0으로 초기화
+    for (let i=0; i < 음료.length; i++){
+        음료[i].선택개수 = 0;
     }
+
 }
 
-// totalPrice 계산함수 calTotalPrice()
-function calTotalPrice(){
-    let temp = 0;
-    for (let i = 0; i < itemNum; i++) {
-        temp += +(drinks[i].price) * +(drinks[i].count);
-    }
-    return temp;
-}
-
-// 음료 선택 취소 기능 함수 cancel(tag)
-function cancel(tag){
-    const countTag = tag.querySelector(".count");
-    let index = drinks.indexOf(drinks.filter(i => i.name == countTag.id)[0]);
-    if(drinks[index].count > 1){
-        drinks[index].count -= 1;
-        drinks[index].stock += 1;
-        // -1된 개수를 다시 찍어주는거
-        countTag.textContent = drinks[index].count;
-    }
-    else if (drinks[index].count == 1){
-        drinks[index].count -= 1;
-        drinks[index].stock += 1;
-        // 아예 li 삭제
-        tag.parentNode.removeChild(tag);
-    }
-    let item_list = machineItems.getElementsByClassName("item-li");
-    for (let i = 0; i < itemNum; i++) {
-        if (drinks[i].stock == 0) {
-            item_list[i].classList.add("soldout");
-        }else if (drinks[i].stock > 0){
-            item_list[i].classList.remove("soldout");
-        }
-    }
-    balance += drinks[index].price;
-    update();
-    alert("상품 선택 취소!");
+function 자금셋팅() {
+    소지금디스플레이.textContent = `${소지금액.toLocaleString()}원`;
+    잔액디스플레이.textContent = `${잔액.toLocaleString()}원`;
+    총금액디스플레이.textContent = `총금액 : ${총금액.toLocaleString()}원`;
 }
